a2de5d2423212cbd67e8572e76d498e8f1067eb6
night37 <47032087+night37@users.noreply.github.com>
2019-12-24 15:26:15 +0100

    drag&drop opérationnel
    drag&drop opérationnel et copie de la bibliotheque vers le depôt

--- src/app/bibliotheque/bibliotheque.component.ts
+++ src/app/bibliotheque/bibliotheque.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, copyArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-bibliotheque',
@@ -7,6 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BibliothequeComponent  {
 
+  depotArticles = [
+    {
+      id: 1,
+      label: "titre1", 
+      content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia blanditiis amet sed doloribus temporibus. Quaerat, quibusdam, incidunt ab amet, esse nostrum perspiciatis perferendis neque eos ad beatae voluptas dolore doloremque."
+    },
+    {
+      id: 2,
+      label: "titre2", 
+      content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia blanditiis amet sed doloribus temporibus. Quaerat, quibusdam, incidunt ab amet, esse nostrum perspiciatis perferendis neque eos ad beatae voluptas dolore doloremque."
+    },    
+    {
+      id: 3,
+      label: "titre3", 
+      content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia blanditiis amet sed doloribus temporibus. Quaerat, quibusdam, incidunt ab amet, esse nostrum perspiciatis perferendis neque eos ad beatae voluptas dolore doloremque."
+    },    
+    {
+      id: 4,
+      label: "titre4", 
+      content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia blanditiis amet sed doloribus temporibus. Quaerat, quibusdam, incidunt ab amet, esse nostrum perspiciatis perferendis neque eos ad beatae voluptas dolore doloremque."
+    },    
+    {
+      id: 5,
+      label: "titre5", 
+      content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia blanditiis amet sed doloribus temporibus. Quaerat, quibusdam, incidunt ab amet, esse nostrum perspiciatis perferendis neque eos ad beatae voluptas dolore doloremque."
+    },    
+    {
+      id: 6,
+      label: "titre6", 
+      content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia blanditiis amet sed doloribus temporibus. Quaerat, quibusdam, incidunt ab amet, esse nostrum perspiciatis perferendis neque eos ad beatae voluptas dolore doloremque."
+    },
+    
+  ];
+  bibliothequeArticles = [
+    
+  ]
 
 
+
+
+
+  
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      
+    } else {
+      copyArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
 }
